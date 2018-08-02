9cc5b4f6a212689aad871ddd1dce851f306a1747
mashudumelton <mashudumelton@gmail.com>
2018-08-02 09:22:39 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MediaCapture
+  
+  ],
+  
 })
 export class AppModule {}
